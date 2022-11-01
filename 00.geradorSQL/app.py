1984d70d9d3cf049a39b7570a50d27f28d9fbed8
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-11-01 20:08:47 -0300

    Gerando SQL para regiao e estados

--- 00.geradorSQL/app.py
+++ 00.geradorSQL/app.py
@@ -9,9 +9,26 @@ estado = linhas[:, :1].flatten().tolist()
 uf = linhas[:, 1:2].flatten().tolist()
 regiao = linhas[:, 2:3].flatten().tolist()
 
-f = open("teste.txt", "w+")
+# print(linhas.flatten())
 
-for i in range(1):
-    e = estado[i]
-    # print(e)
-    f.write(e)
+
+# INSERT INTO Regiao(regiao)VALUES($"Variable")
+# Removendo nomes iguais
+regiao = list(set(regiao))
+
+f = open("teste.txt", "w+")
+for i in range(len(regiao)):
+    e = regiao[i]
+    f.write("INSERT INTO Regiao(regiao)VALUES('" + e + "')" + "\n")
+
+aux = ""
+linha_estado = ""
+linha_UF = ""
+idRegiao = 0
+for i in range(len(linhas)):
+    linha = linhas[i].tolist()
+    if (linha[2] != aux):
+        idRegiao = idRegiao + 1
+    f.write("INSERT INTO Estado(estado, UF, idRegiao)VALUES('" +
+            linha[0] + "," + linha[1] + "," + str(idRegiao) + "')" + "\n")
+    aux = linha[2]
