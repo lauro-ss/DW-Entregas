86476c63e333773e6441e3081705a5b8a45c0c6b
Lauro Santana Silva <bestlaurobr@gmail.com>
2022-11-01 20:38:42 -0300

    Adicionado gerador para municipios

--- 00.geradorSQL/app.py
+++ 00.geradorSQL/app.py
@@ -1,13 +1,14 @@
 import pandas as pd
-import numpy as nd
 
-csv = 'estados.csv'
+csv_estados = 'estados.csv'
+csv_municipios = 'municipios.csv'
 
-linhas = pd.read_csv(csv, sep=';').values
+linhas_estado = pd.read_csv(csv_estados, sep=';').values
+linhas_municipio = pd.read_csv(csv_municipios, sep=';').values
 
-estado = linhas[:, :1].flatten().tolist()
-uf = linhas[:, 1:2].flatten().tolist()
-regiao = linhas[:, 2:3].flatten().tolist()
+#estado = linhas[:, :1].flatten().tolist()
+#uf = linhas[:, 1:2].flatten().tolist()
+regiao = linhas_estado[:, 2:3].flatten().tolist()
 
 # print(linhas.flatten())
 
@@ -21,14 +22,22 @@ for i in range(len(regiao)):
     e = regiao[i]
     f.write("INSERT INTO Regiao(regiao)VALUES('" + e + "')" + "\n")
 
-aux = ""
-linha_estado = ""
-linha_UF = ""
+aux_regiao = ""
 idRegiao = 0
-for i in range(len(linhas)):
-    linha = linhas[i].tolist()
-    if (linha[2] != aux):
+for i in range(len(linhas_estado)):
+    linha_estado = linhas_estado[i].tolist()
+    if (linha_estado[2] != aux_regiao):
         idRegiao = idRegiao + 1
     f.write("INSERT INTO Estado(estado, UF, idRegiao)VALUES('" +
-            linha[0] + "," + linha[1] + "," + str(idRegiao) + "')" + "\n")
-    aux = linha[2]
+            linha_estado[0] + "," + linha_estado[1] + "," + str(idRegiao) + "')" + "\n")
+    aux_regiao = linha_estado[2]
+
+aux_estado = ""
+idEstado = 0
+for i in range(len(linhas_municipio)):
+    linha_municipio = linhas_municipio[i].tolist()
+    if (linha_municipio[0] != aux_estado):
+        idEstado = idEstado + 1
+    f.write("INSERT INTO Cidade(cidade, idEstado)VALUES('" +
+            linha_municipio[1] + "," + str(idEstado) + "')" + "\n")
+    aux_estado = linha_municipio[0]
